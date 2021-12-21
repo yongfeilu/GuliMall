a29f5ea01b43e89dd3ccdbdd155062f5037bb05a
yongfeilu <yflecon@gmail.com>
2021-12-22 07:34:29 +0800

    initial structure created

---
+++ gulimall-member/src/test/java/com/atguigu/gulimall/lmember/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.lmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
